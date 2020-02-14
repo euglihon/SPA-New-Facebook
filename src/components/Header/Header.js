3eabdfa1be8c56b5b9163370adeaba070995314b
Eugene Lihon <eug.lihon@hotmail.com>
2020-02-14 13:26:49 +0100

    init project and create basic components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F0%2F06%2FJIS_College_of_Engineering_Logo.png&f=1&nofb=1' />
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './Profile.css';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.ucdmc.ucdavis.edu%2Femergency%2Fimages%2Fbody%2Fwilderness%2Fwilderness_wide_01.jpg&f=1&nofb=1' />
+      </div>
+        
+      <div>
+        avatar + descriptions
+      </div>
+
+      <div>
+        My posts
+      <div>
+        new post
+      </div>
+
+      <div>post 1</div>
+        <div>post 2</div>
+        <div>post 3</div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
