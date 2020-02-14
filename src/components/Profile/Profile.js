ae4cc9045aa48c9352ae3fc5363df19443fa4cea
Eugene Lihon <eug.lihon@hotmail.com>
2020-02-14 21:34:45 +0100

    edit classes Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
   return (
-    <div className={classes.content}>
+    <div>
       <div>
         <img alt='#' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.ucdmc.ucdavis.edu%2Femergency%2Fimages%2Fbody%2Fwilderness%2Fwilderness_wide_01.jpg&f=1&nofb=1' />
       </div>
