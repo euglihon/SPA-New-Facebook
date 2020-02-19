a5c2ad05a315fbd96bf71f636507827e854e97a8
Eugene Lihon <eug.lihon@hotmail.com>
2020-02-19 19:13:28 +0100

    add render.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 import state from './redux/state';
+import { renderAllTree } from './render';
 
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-const postsData = [
-  {id: 1, messageText: 'Hello New-Facebook', likesCount: 10},
-  {id: 2, messageText: 'Hello React', likesCount: 5},
-];
-
-const dialogData = [
-  {id: 1, name: 'Zeka'},
-  {id: 2, name: 'Andrey'},
-  {id: 3, name: 'Kolek'}
-];
-
-const messageData = [
-  {id: 1, messageText: 'Hello'},
-  {id: 2, messageText: 'It\'s my first message'},
-  {id:3, messageText: 'lol'}
-];
-
-ReactDOM.render(<App state={state}/>, document.getElementById('root'));
-
-serviceWorker.unregister();
+renderAllTree(state);
