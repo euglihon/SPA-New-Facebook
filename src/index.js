6e535d3a9b204cef5f4c1ea72b7525d422362832
Eugene Lihon <eug.lihon@hotmail.com>
2020-02-25 22:41:02 +0100

    add OOP Store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import state from './redux/state';
 import App from './App';
-import {addPost, addTextareaValue, subscribe} from './redux/state';
+import store from './redux/state';
 
 
 const renderTree = () => {
-  ReactDOM.render(<App state={state} addPost={addPost} addTextareaValue={addTextareaValue}/>, document.getElementById('root'));
+  ReactDOM.render(<App state={store.getState()} addPost={store.addPost.bind(store)} addTextareaValue={store.addTextareaValue.bind(store)}/>, document.getElementById('root'));
 }
 
-renderTree(state);
-subscribe(renderTree);
+renderTree(store.getState());
+store.subscribe(renderTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,65 +1,70 @@
-let reRender = () => {
-  console.log('reRenderTree');
-}
+const store = {
 
-const state = {
+  _state: {
 
-  profilePage: {
+    profilePage: {
 
-    posts: [
-      {id: 1, messageText: 'Hello New-Facebook', likesCount: 10},
-      {id: 2, messageText: 'Hello React', likesCount: 5},
-    ],
-    textareaValue: '',
-  },
+      posts: [
+        {id: 1, messageText: 'Hello New-Facebook', likesCount: 10},
+        {id: 2, messageText: 'Hello React', likesCount: 5},
+      ],
+      textareaValue: '',
+    },
 
-  dialogsPage: {
+    dialogsPage: {
 
-    dialogs: [
-      {id: 1, name: 'Zeka'},
-      {id: 2, name: 'Andrey'},
-      {id: 3, name: 'Kolek'}
-    ],
+      dialogs: [
+        {id: 1, name: 'Zeka'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Kolek'}
+      ],
 
-    messagesIn: [
-      {id: 1, messageText: 'Hello'},
-      {id: 2, messageText: 'It\'s my first message'},
-      {id: 3, messageText: 'lol'}
-    ],
+      messagesIn: [
+        {id: 1, messageText: 'Hello'},
+        {id: 2, messageText: 'It\'s my first message'},
+        {id: 3, messageText: 'lol'}
+      ],
 
-    messagesOut: [
-      {id: 1, messageText: 'Hello React'},
-      {id: 2, messageText: 'Ok'},
-      {id: 3, messageText: 'Bye Bye'}
+      messagesOut: [
+        {id: 1, messageText: 'Hello React'},
+        {id: 2, messageText: 'Ok'},
+        {id: 3, messageText: 'Bye Bye'}
+      ]
+    },
+
+    friends: [
+      {id: 1, name: 'Oleg'},
+      {id: 2, name: 'Vanek'},
+      {id: 3, name: 'Kolek'},
+      {id: 4, name: 'Igor'},
+      {id: 5, name: 'Vasia'},
+      {id: 6, name: 'Pitr'},
+      {id: 7, name: 'Serega'}
     ]
   },
 
-  friends: [
-    {id: 1, name: 'Oleg'},
-    {id: 2, name: 'Vanek'},
-    {id: 3, name: 'Kolek'},
-    {id: 4, name: 'Igor'},
-    {id: 5, name: 'Vasia'},
-    {id: 6, name: 'Pitr'},
-    {id: 7, name: 'Serega'}
-  ]
+  getState() {
+    return this._state;
+  },
+
+  _callSubscriber() {
+    console.log('reRenderTree');
+  },
+
+  addPost() {
+    const newPost = { id: 3, messageText: this._state.profilePage.textareaValue, likesCount:0 }
+    this._state.profilePage.posts.push(newPost);
+    this._callSubscriber();
+  },
+
+  addTextareaValue(newValue) {
+    this._state.profilePage.textareaValue = newValue;
+    this._callSubscriber();
+   },
+
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 }
 
-window.state = state;
-
-export const addPost = () => {
-  const newPost = { id: 3, messageText: state.profilePage.textareaValue, likesCount:0 }
-  state.profilePage.posts.push(newPost);
-  reRender();
-}
-
-export const addTextareaValue = (newValue) => {
-  state.profilePage.textareaValue = newValue;
-  reRender();
- }
-
-export const subscribe = (observer) => {
-  reRender = observer;
-}
-
-export default state;
+export default store;
