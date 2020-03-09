177a86bb1a7942961b5bc15aa9ab72ff20a89c3b
Eugene Lihon <eug.lihon@hotmail.com>
2020-03-09 20:01:25 +0100

    init usersReducer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import {createStore, combineReducers} from 'redux';
 import dialogReducer from './dialogsReducer';
 import profileReducer from './profileReducer';
 import sideBarReducer from './sideBarReducer';
+import usersReducer from './usersReducer';
 
 
 const reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogReducer,
+  usersPage: usersReducer,
   sideBar: sideBarReducer
 });
 
---
+++ src/redux/usersReducer.js
@@ -0,0 +1,60 @@
+const initialState = {
+  users: [
+    {id: 1, followed: false, name: 'Zeka', status: 'Welcome', location: {city: 'Warsaw', country: 'Poland' }, photoUrl: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-RhIEzw81SvY%2FT7ZGODCyT1I%2FAAAAAAAAAG4%2FO8EXS_Apd10%2Fs1600%2Fcobaki_08.jpg&f=1&nofb=1' },
+
+    {id: 2, followed: false, name: 'Ivan', status: 'I live in Russia', location: {city: 'Moscow', country: 'Russia' }, photoUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F7%2F7c%2FYl02.jpg&f=1&nofb=1' },
+
+    {id: 3, followed: true, name: 'Andrew', status: 'Hello friends', location: {city: 'Barcelona', country: 'Spain' }, photoUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FRZqa9nXbLgY%2Fmaxresdefault.jpg&f=1&nofb=1' },
+  ]
+};
+
+
+const usersReducer = (state = initialState, action) => {
+  if (action.type === 'FOLLOW') {
+    const copyState = {...state};
+    copyState.users = [...state.users];
+    if (copyState.users.id === action.userId) {
+      copyState.users.followed = true;
+    }
+    return copyState;
+  }
+
+  else if (action.type === 'UNFOLLOW') {
+    const copyState = {...state};
+    copyState.users = [...state.users];
+    if (copyState.users.id === action.userId) {
+      copyState.users.followed = false;
+    }
+    return copyState;
+  }
+
+  else if (action.type === 'SET-USERS') {
+    const copyState = {...state};
+    copyState.users = [...state.users, ...action.users]; //add users
+  }
+  return state;
+};
+
+
+export const followActionCreator = (userId) => {
+  return {
+    type: 'FOLLOW',
+    userId: userId
+  }
+}
+
+export const unFollowActionCreator = (userId) => {
+  return {
+    type: 'UNFOLLOW',
+    userId: userId
+  }
+}
+
+export const setUsersActionCreation = (users) => {
+  return {
+    type: 'SET-USERS',
+    users: users
+  }
+};
+
+export default usersReducer;
