432f32c80fe9a7a1dcaea0d85c3c64994b4b40f6
Eugene Lihon <eug.lihon@hotmail.com>
2020-03-20 19:51:37 +0100

    add new AC and Reducer items

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,13 +1,9 @@
 const initialState = {
-  users: [
-    // {id: 1, followed: false, name: 'Zeka', status: 'Welcome', location: {city: 'Warsaw', country: 'Poland' }, photoUrl: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-RhIEzw81SvY%2FT7ZGODCyT1I%2FAAAAAAAAAG4%2FO8EXS_Apd10%2Fs1600%2Fcobaki_08.jpg&f=1&nofb=1' },
-
-    // {id: 2, followed: false, name: 'Ivan', status: 'I live in Russia', location: {city: 'Moscow', country: 'Russia' }, photoUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F7%2F7c%2FYl02.jpg&f=1&nofb=1' },
-
-    // {id: 3, followed: true, name: 'Andrew', status: 'Hello friends', location: {city: 'Barcelona', country: 'Spain' }, photoUrl: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FRZqa9nXbLgY%2Fmaxresdefault.jpg&f=1&nofb=1' },
-  ]
+  users: [],
+  pageSize: 20,
+  totalUsersCount: 0,
+  currentPage: 1
 };
-
 
 const usersReducer = (state = initialState, action) => {
   if (action.type === 'FOLLOW') {
@@ -36,9 +32,22 @@ const usersReducer = (state = initialState, action) => {
 
   else if (action.type === 'SET-USERS') {
     const copyState = {...state};
-    copyState.users = [...state.users, ...action.users]; //add users
+    copyState.users = [...action.users]; //add users
     return copyState;
   }
+
+  else if (action.type === 'SET-CURRENT-PAGE') {
+    const copyState = {...state};
+    copyState.currentPage = action.page;
+    return copyState;
+  }
+
+  else if (action.type === 'SET-TOTAL-USERS-COUNT') {
+    const copyState = {...state}
+    copyState.totalUsersCount = action.totalUsers;
+    return copyState;
+  }
+
   return state;
 };
 
@@ -63,5 +72,19 @@ export const setUsersActionCreation = (users) => {
     users: users
   }
 };
+
+export const setCurrentPageActionCreation = (page) => {
+  return {
+    type: 'SET-CURRENT-PAGE',
+    page: page
+  }
+}
+
+export const setTotalUserCountActionCreation = (totalUsers) => {
+  return {
+    type: 'SET-TOTAL-USERS-COUNT',
+    totalUsers: totalUsers
+  }
+}
 
 export default usersReducer;
