7418b6ef78a581b6f66df22eeaa2b7e062a930a6
Eugene Lihon <eug.lihon@hotmail.com>
2020-03-23 19:58:46 +0100

    add profile page users information

--- src/components/Profile/ProfInfo/ProfInfo.js
+++ src/components/Profile/ProfInfo/ProfInfo.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import classes from './ProfInfo.module.css';
+import Preloader from '../../common/Preloader/Preloader';
+import nullProfileImg from '../../../assets/images/user-null.png'
 
-const ProfInfo = () => {
-  return (
-    <div>
+const ProfInfo = (props) => {
+  if (props.profile === null || undefined) {
+    return (
+      <Preloader />
+    )
+  } else {
+    return (
       <div>
-        <img alt='#' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.ucdmc.ucdavis.edu%2Femergency%2Fimages%2Fbody%2Fwilderness%2Fwilderness_wide_01.jpg&f=1&nofb=1' />
-      </div>
+        <div>
+          <img alt='#' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.ucdmc.ucdavis.edu%2Femergency%2Fimages%2Fbody%2Fwilderness%2Fwilderness_wide_01.jpg&f=1&nofb=1' />
+        </div>
 
-      <div>
-        avatar + descriptions
+        <div className={classes.infoBlock}>
+          <div>
+            <img className={classes.profileImg} alt='profile image'
+              src={
+                props.profile.photos.large != null || undefined
+                  ? props.profile.photos.large
+                  : nullProfileImg
+              }
+            />
+          </div>
+
+          <div className={classes.contactBlock}>
+            <h1>{props.profile.fullName}</h1>
+            <p>{props.profile.aboutMe}</p>
+
+            <div className={classes.links}>
+              <div>Contacts</div>
+              <a href={props.profile.contacts.github}>GitHub</a>
+              <a href={props.profile.contacts.facebook}>Facebook</a>
+              <a href={props.profile.contacts.vk}>VK</a>
+            </div>
+          </div>
+        </div>
       </div>
-  </div>
-  )
+    )
+  }
 }
 
 export default ProfInfo;
