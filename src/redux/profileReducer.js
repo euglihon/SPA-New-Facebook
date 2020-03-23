ded8bba598992435a423bbd4c59795b392dfa848
Eugene Lihon <eug.lihon@hotmail.com>
2020-03-23 20:00:10 +0100

    add profile info in  Reducer

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -3,37 +3,33 @@ const initialState = {
     {id: 1, messageText: 'Hello New-Facebook', likesCount: 10},
     {id: 2, messageText: 'Hello React', likesCount: 5}
   ],
-
-  textareaValue: ''
+  textareaValue: '',
+  profile: null
 };
-
 
 const profileReducer = (state = initialState, action) => {
 
   if (action.type === 'ADD-POST') {
     const copyState = {...state};    //глубокие копии
     copyState.posts = [...state.posts];  //глубокие копии
-
     const newPost = { id: 3, messageText: copyState.textareaValue, likesCount:0 };
     copyState.posts.push(newPost);
-
     return copyState
   }
 
   else if (action.type === 'ADD-TEXTAREA-VALUE') {
     const copyState = {...state};   //глубокие копии
-
     copyState.textareaValue = action.newValue;
-
     return copyState;
   }
 
+  else if (action.type === 'SET-USER-PROFILE') {
+    const copyState = {...state};
+    copyState.profile = action.profile;
+    return copyState;
+  }
   return state;
 };
-
-
-
-
 
 
 export const addPostActionCreator = () => {
@@ -46,6 +42,13 @@ export const updateNewValueActionCreator = (text) => {
   return {
     type: 'ADD-TEXTAREA-VALUE',
     newValue: text
+  }
+};
+
+export const setUserProfile = (profile) => {
+  return {
+    type: 'SET-USER-PROFILE',
+    profile: profile
   }
 };
 
