605ae7e3c187c6f13b0c4727727434dce1b87229
Eugene Lihon <eug.lihon@hotmail.com>
2020-03-24 20:59:44 +0100

    add user name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 import classes from './Header.module.css';
 
+const Header = (props) => {
 
-const Header = () => {
+  debugger;
   return (
     <header className={classes.header}>
       <img alt='#' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F0%2F06%2FJIS_College_of_Engineering_Logo.png&f=1&nofb=1' />
+
+      <div className={classes.loginBlock}>
+        {
+          props.isAuth === true
+          ? <NavLink to={'/login'}>Login</NavLink>
+          : <b>{props.loginName}</b>
+        }
+      </div>
+
     </header>
   )
 }
