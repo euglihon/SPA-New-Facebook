91dc181b8dcd0d02c7702d7f6bb4a11412c806fb
Eugene Lihon <eug.lihon@hotmail.com>
2020-03-24 20:58:50 +0100

    add authReducer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,22 +3,18 @@ import dialogReducer from './dialogsReducer';
 import profileReducer from './profileReducer';
 import sideBarReducer from './sideBarReducer';
 import usersReducer from './usersReducer';
-
+import authReducer from './authReducer';
 
 const reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogReducer,
   usersPage: usersReducer,
-  sideBar: sideBarReducer
+  sideBar: sideBarReducer,
+  auth: authReducer
 });
-
-
 
 const store = createStore(reducers);
 
-
 window.store = store;
-
-
 
 export default store;
