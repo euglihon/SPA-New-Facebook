52a678cd234bd44adec2979d1bf674397611259b
Eugene Lihon <eug.lihon@hotmail.com>
2020-03-26 18:44:47 +0100

    add API file

---
+++ src/api/api.js
@@ -0,0 +1,47 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  withCredentials: true,
+  headers: {
+    'API-KEY': '1aa28f91-66e0-43de-9c03-07fbb8ee7bd0'
+  }
+})
+
+export const usersAPI = {
+  getUsers(currentPage, pageSize) {
+    return (
+      instance
+        .get(`users?page=${currentPage}&count=${pageSize}`)
+        .then( (response) => response.data )
+    )
+  }
+}
+
+export const profileAPI = {
+  getProfile(userId) {
+    return (
+      instance
+        .get(`profile/${userId}`)
+        .then( (response) => response.data)
+    )
+  }
+}
+
+export const followUnFolowAPI = {
+  deleteFollow(userId) {
+    return (
+      instance
+        .delete(`follow/${userId}`)
+        .then( (response) => response.data.resultCode)
+    )
+  },
+
+  postFollow(userId) {
+    return (
+      instance
+        .post(`follow/${userId}`, {})
+        .then( (response) => response.data.resultCode)
+    )
+  }
+}
